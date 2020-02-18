045812d556da8ccf514601157e5adb32bb0e8284
gitjoao <joao.moraes.batista@hotmail.com>
2020-02-18 10:05:40 -0300

    Updates

---
+++ precache-manifest.d13a3d4f831d24eb553585b14fd76069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9b62e32db43fa3e703a11b28c878de",
+    "url": "/react-chucknorris/index.html"
+  },
+  {
+    "revision": "51a6adab42e8c6a7fe8b",
+    "url": "/react-chucknorris/static/css/main.572dc0b3.chunk.css"
+  },
+  {
+    "revision": "c7565c1d527f78e2c698",
+    "url": "/react-chucknorris/static/js/2.9832c33f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-chucknorris/static/js/2.9832c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a6adab42e8c6a7fe8b",
+    "url": "/react-chucknorris/static/js/main.b9a95629.chunk.js"
+  },
+  {
+    "revision": "1b569001f38d98fce437",
+    "url": "/react-chucknorris/static/js/runtime-main.108d608c.js"
+  }
+]);
